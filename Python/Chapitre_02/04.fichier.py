77e69d15f82227216517488907bf32ac26c3c5f7
YassAje <94759159+YassAje@users.noreply.github.com>
2021-12-08 01:07:46 +0100

    Delete Python directory

--- Python/Chapitre_02/04.fichier.py
+++
@@ -1,9 +0,0 @@
-try:
-    f = open('infos.txt', 'r')
-    for l in f.readlines():
-        print(int(l.strip()))
-except:
-    print("erreur")
-finally:
-    f.close
-    
--- Python/Chapitre_02/regarder.py
+++
@@ -1,9 +0,0 @@
-from Television.SerieTele import EpisodeDrWho 
-
-ep = EpisodeDrWho(7, 5)
-
-ep.attaque_dalek()
-ep.diffuser()
-
-
-__init__.py
